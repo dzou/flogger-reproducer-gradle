1ad5abfae7efa4e8672cfb893b612e0e5cbad54f
dzou <dzou@users.noreply.github.com>
2021-10-18 15:30:45 -0400

    flogger issue reproducer

---
+++ src/main/java/com/example/Driver.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import com.google.common.flogger.backend.system.DefaultPlatform;
+
+public class Driver {
+  public static void main(String[] args) {
+    System.out.println("Hello");
+    new DefaultPlatform();
+    System.out.println("Hello");
+  }
+}
